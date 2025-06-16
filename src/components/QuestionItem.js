f3000cc42982c64916c8cc33f1b5cb15f7fb0161
Mike <mclancy96@gmail.com>
2025-06-16 15:06:37 -0400

    Update correct answer on click

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,32 +1,47 @@
 import React from "react";
 
 function QuestionItem({ question, setQuestions }) {
-  const { id, prompt, answers, correctIndex } = question;
+	const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+	const options = answers.map((answer, index) => (
+		<option key={index} value={index}>
+			{answer}
+		</option>
+	));
 
-  const handleDelete = (event) => {
-    event.preventDefault()
-    fetch(`http://localhost:4000/questions/${id}`, { method: 'DELETE', headers: { "Accepts": "application/json" }, })
-      .then(r => r.json())
-      .then(setQuestions(questions => questions.filter(q => q.id !== id)))
-  }
+	const handleDelete = (event) => {
+		event.preventDefault();
+		fetch(`http://localhost:4000/questions/${id}`, {
+			method: "DELETE",
+			headers: { Accepts: "application/json" },
+		})
+			.then((r) => r.json())
+			.then(setQuestions((questions) => questions.filter((q) => q.id !== id)));
+	};
 
-  return (
-    <li>
-      <h4>Question {id}</h4>
-      <h5>Prompt: {prompt}</h5>
-      <label>
-        Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
-      </label>
-      <button onClick={handleDelete}>Delete Question</button>
-    </li>
-  );
+	const updateCorrectAnswer = (e) => {
+		fetch(`http://localhost:4000/questions/${id}`, {
+			method: "PATCH",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ correctIndex: Number.parseInt(e.target.value) }),
+		})
+			.then((r) => r.json())
+			.then(setQuestions((questions) => questions));
+	};
+
+	return (
+		<li>
+			<h4>Question {id}</h4>
+			<h5>Prompt: {prompt}</h5>
+			<label>
+				Correct Answer:
+				<select defaultValue={correctIndex} onChange={updateCorrectAnswer}>
+					{options}
+				</select>
+			</label>
+			<button onClick={handleDelete}>Delete Question</button>
+		</li>
+	);
 }
 
 export default QuestionItem;
